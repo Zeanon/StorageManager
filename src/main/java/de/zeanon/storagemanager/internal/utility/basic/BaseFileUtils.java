fe55d66daed8fc24d3b9edc4ae244babb1bc2800
Zeanon <thezeanon@gmail.com>
2020-02-02 22:58:31 +0100

    Bugfix
    Signed-off-by: Zeanon <thezeanon@gmail.com>

--- src/main/java/de/zeanon/storagemanager/internal/utility/basic/BaseFileUtils.java
+++ src/main/java/de/zeanon/storagemanager/internal/utility/basic/BaseFileUtils.java
@@ -826,7 +826,7 @@ public class BaseFileUtils {
 		if (dotInd <= sepInd) {
 			return "";
 		} else {
-			return filePath.substring(dotInd + 1).toLowerCase();
+			return filePath.substring(dotInd + 1);
 		}
 	}
 
@@ -874,7 +874,7 @@ public class BaseFileUtils {
 		if (dotInd <= sepInd) {
 			return filePath;
 		} else {
-			return filePath.substring(0, dotInd).toLowerCase();
+			return filePath.substring(0, dotInd);
 		}
 	}
 
