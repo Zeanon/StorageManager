0ccca42b0dd33464b2b5a39b1fb90aad760f6ace
Zeanon <thezeanon@gmail.com>
2023-12-16 14:24:33 +0100

    .

--- YapionFileManager/src/main/java/de/zeanon/yapionfilemanager/YapionFileManager.java
+++
@@ -1,5 +0,0 @@
-package de.zeanon.yapionfilemanager;
-
-public abstract class YapionFileManager {
-
-}
--- YapionFileManager/src/main/java/de/zeanon/yapionfilemanager/internal/files/raw/YapionFile.java
+++
@@ -1,8 +0,0 @@
-package de.zeanon.yapionfilemanager.internal.files.raw;
-
-import de.zeanon.storagemanagercore.internal.base.files.FlatFile;
-
-
-public class YapionFile extends FlatFile {
-
-}
